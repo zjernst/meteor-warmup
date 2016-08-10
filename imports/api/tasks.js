a1b58fd6c0368235a9190419acb4e9ae54563c76
zjernst <zjernst@gmail.com>
2016-08-10 15:21:59 -0700

    setting up testing

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -6,7 +6,12 @@ export const Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isServer) {
   Meteor.publish('tasks', function tasksPublication() {
-    return Tasks.find();
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true} },
+        { owner: this.userId },
+      ]
+    });
   })
 }
 
@@ -28,11 +33,21 @@ Meteor.methods({
   'tasks.remove'(taskId) {
     check(taskId, String);
 
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
     Tasks.remove(taskId);
   },
   'tasks.setChecked'(taskId, setChecked) {
     check(taskId, String);
     check(setChecked, Boolean);
+
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
 
     Tasks.update(taskId, { $set: { checked: setChecked } });
   },
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
 
 // Task component - represents a single todo item
 export default class Task extends Component {
@@ -16,7 +17,10 @@ export default class Task extends Component {
   }
 
   render() {
-    const taskClassName = this.props.task.checked ? 'checked' : '';
+    const taskClassName = classnames({
+      checked: this.props.task.checked,
+      private: this.props.task.private,
+    });
 
     return (
       <li className={taskClassName}>
