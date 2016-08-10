bc922777495093507e7177999bf6245a97944400
zjernst <zjernst@gmail.com>
2016-08-09 19:37:23 -0700

    update and delete tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
@@ -7,13 +8,18 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
-  // getTasks() {
-  //   return [
-  //     { _id: 1, text: 'This is task 1' },
-  //     { _id: 2, text: 'This is task 2' },
-  //     { _id: 3, text: 'This is task 3' },
-  //   ];
-  // }
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
 
   renderTasks() {
     return this.props.tasks.map((task) => (
@@ -26,6 +32,14 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
 
         <ul>
@@ -42,6 +56,6 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 }, App);
