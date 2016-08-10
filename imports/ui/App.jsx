1acdb36862cb1c6cf7d0e09113f7c307c5be230c
zjernst <zjernst@gmail.com>
2016-08-10 14:07:08 -0700

    secured data

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -23,12 +23,7 @@ class App extends Component {
 
     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
 
-    Tasks.insert({
-      text,
-      createdAt: new Date(),
-      owner: Meteor.userId(),
-      username: Meteor.user().username,
-    });
+    Meteor.call('tasks.insert', text);
 
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
