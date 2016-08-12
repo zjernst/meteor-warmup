34977bb46a41551a6b4af95b0ecf60cf26feb2af
zjernst <zjernst@gmail.com>
2016-08-11 23:06:39 -0700

    adding whatsapp start

---
+++ whatsapp/client/scripts/lib/app.js
@@ -0,0 +1,36 @@
+import 'angular-animate';
+import 'angular-meteor';
+import 'angular-sanitize';
+import 'angular-ui-router';
+import 'ionic-scripts';
+import Angular from 'angular';
+import Loader from 'angular-ecmascript/module-loader';
+import { Meteor } from 'meteor/meteor';
+
+// modules
+import RoutesConfig from '../routes';
+import ChatsCtrl from '../controllers/chats.controller';
+import CalendarFilter from '../filters/calendar.filter';
+
+const App = 'Whatsapp';
+
+Angular.module(App, [
+  'angular-meteor',
+  'ionic'
+]);
+
+new Loader(App)
+  .load(ChatsCtrl)
+  .load(CalendarFilter)
+  .load(RoutesConfig);
+
+// startup
+if (Meteor.isCordova) {
+  Angular.element(document).on('deviceready', onReady);
+} else {
+  Angular.element(document).ready(onReady);
+}
+
+function onReady() {
+  Angular.bootstrap(document, [App]);
+}
